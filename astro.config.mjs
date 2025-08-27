c1a7a62af0a753d5fd253773f7d21950a9570d04
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-08-28 00:17:49 +0300

    added starlight next.js theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeNext from 'starlight-theme-next';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
+            plugins: [starlightThemeNext()],
 			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
