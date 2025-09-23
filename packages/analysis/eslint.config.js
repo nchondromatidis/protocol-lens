bda987bd772832741d911325c7b5c93a3d64c9f0
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-09-23 11:02:12 +0300

    refactor: moved ts, eslint, prettier, vite config files into their own repo

--- packages/analysis/eslint.config.js
+++ packages/analysis/eslint.config.js
@@ -1,28 +1,4 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import config from "@defi-notes/config/eslint.config";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-      eslintPluginPrettierRecommended,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    rules: {
-      "@typescript-eslint/no-explicit-any)": "warn",
-    }
-  },
-])
+/** @type {import("eslint").Linter.Config} */
+export default config;
