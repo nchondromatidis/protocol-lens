770148df4a675830c54c8a0554b899cafd1faf28
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-10-15 00:49:25 +0300

    updated Makefile to work with pnpm and removed sourcemaps task

--- packages/protocols/src/types.ts
+++ packages/protocols/src/types.ts
@@ -1,4 +1,4 @@
 export type { ArtifactMap, LinkReferences, ImmutableReferences } from 'hardhat/types/artifacts';
 
-import type { Artifact as HardhatArtifact } from 'hardhat/types/artifacts';
-export type ProtocolArtifact = HardhatArtifact & { bytecodeSourceMap: string; deployedBytecodeSourceMap: string };
+import type { Artifact } from 'hardhat/types/artifacts';
+export type ProtocolArtifact = Artifact;
