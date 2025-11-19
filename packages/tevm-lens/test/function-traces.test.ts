39628b2f03e49e0bcd446bec72dfd6c0abd4c789
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-11-19 23:13:22 +0200

    tevm-lens: updated tests to run in parallel

--- packages/tevm-lens/test/function-traces.test.ts
+++ packages/tevm-lens/test/function-traces.test.ts
@@ -1,7 +1,6 @@
-import { test, beforeEach, beforeAll } from 'vitest';
+import { test, beforeEach, describe } from 'vitest';
 import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
 import { tevmSetAccount } from 'tevm';
-import { type Vm } from 'tevm/vm';
 import { LensClient } from '../src/lens/LensClient.ts';
 import { buildClient } from '../src/lens/client.ts';
 import { TestResourceLoader } from './setup/TestResourceLoader.ts';
@@ -17,130 +16,123 @@ import type { ProtocolName } from './setup/artifacts/index.js';
 import { deployFunctionTracesContracts } from './setup/function-traces/deploy.js';
 import type { Hex } from '../src/lens/types/artifact.js';
 
-let lensClient: LensClient<FunctionTracesArtifactsMap>;
-let callerContract: Awaited<ReturnType<typeof deployFunctionTracesContracts>>['callerContract'];
-// let calleeContract: Awaited<ReturnType<typeof deployFunctionTracesContracts>>['calleeContract'];
-let vm: Vm;
-let client: Awaited<ReturnType<typeof buildClient>>;
-let resourceLoader: IResourceLoader<FunctionTracesArtifactsMap, ProtocolName>;
+describe('function traces', () => {
+  let lensClient: LensClient<FunctionTracesArtifactsMap>;
+  let callerContract: Awaited<ReturnType<typeof deployFunctionTracesContracts>>['callerContract'];
+  // let calleeContract: Awaited<ReturnType<typeof deployFunctionTracesContracts>>['calleeContract'];
+  let client: Awaited<ReturnType<typeof buildClient>>;
+  let resourceLoader: IResourceLoader<FunctionTracesArtifactsMap, ProtocolName>;
 
-beforeAll(async () => {
-  const deployerAccount = privateKeyToAccount(generatePrivateKey());
+  beforeEach(async () => {
+    const deployerAccount = privateKeyToAccount(generatePrivateKey());
 
-  client = await buildClient(deployerAccount);
+    client = await buildClient(deployerAccount);
 
-  const artifactsPath = path.join(__dirname, 'setup', 'artifacts');
-  const artifactsContractsPath = path.join(__dirname, 'setup', 'artifacts', 'contracts');
-  resourceLoader = new TestResourceLoader<FunctionTracesArtifactsMap, ProtocolName>(
-    artifactsPath,
-    artifactsContractsPath
-  );
+    const artifactsPath = path.join(__dirname, 'setup', 'artifacts');
+    const artifactsContractsPath = path.join(__dirname, 'setup', 'artifacts', 'contracts');
+    resourceLoader = new TestResourceLoader<FunctionTracesArtifactsMap, ProtocolName>(
+      artifactsPath,
+      artifactsContractsPath
+    );
 
-  const supportedContracts = new SupportedContracts<FunctionTracesArtifactsMap>();
-  const labeledContracts = new DeployedContracts<FunctionTracesArtifactsMap>();
-  const tracer = new LensCallTracer<FunctionTracesArtifactsMap>(supportedContracts, labeledContracts);
-  lensClient = new LensClient<FunctionTracesArtifactsMap>(client, supportedContracts, labeledContracts, tracer);
+    const supportedContracts = new SupportedContracts<FunctionTracesArtifactsMap>();
+    const labeledContracts = new DeployedContracts<FunctionTracesArtifactsMap>();
+    const tracer = new LensCallTracer<FunctionTracesArtifactsMap>(supportedContracts, labeledContracts);
+    lensClient = new LensClient<FunctionTracesArtifactsMap>(client, supportedContracts, labeledContracts, tracer);
 
-  const artifacts = await resourceLoader.getProtocolArtifacts('function-traces');
-  await supportedContracts.registerArtifacts(artifacts);
+    const artifacts = await resourceLoader.getProtocolArtifacts('function-traces');
+    await supportedContracts.registerArtifacts(artifacts);
 
-  const functionIndexes = await resourceLoader.getFunctionIndexes('function-traces');
-  await supportedContracts.registerFunctionIndexes(functionIndexes);
+    const functionIndexes = await resourceLoader.getFunctionIndexes('function-traces');
+    await supportedContracts.registerFunctionIndexes(functionIndexes);
 
-  await tevmSetAccount(lensClient.client, {
-    address: deployerAccount.address,
-    balance: ETHER_1,
+    await tevmSetAccount(lensClient.client, {
+      address: deployerAccount.address,
+      balance: ETHER_1,
+    });
+
+    const deployment = await deployFunctionTracesContracts(lensClient, resourceLoader);
+    callerContract = deployment.callerContract;
   });
 
-  const deployment = await deployFunctionTracesContracts(lensClient, resourceLoader);
-  callerContract = deployment.callerContract;
-  // calleeContract = deployment.calleeContract;
+  test('state', async () => {
+    await lensClient.contract(callerContract, 'callExternalFunction', []);
+    const txHash = lensClient.callDecodeTracer.succeededTxs.keys().next().value;
+    const callTraceResult = await client.transport.tevm.request({
+      method: 'debug_traceTransaction',
+      params: [
+        {
+          transactionHash: txHash,
+          tracer: 'callTracer',
+          tracerConfig: { onlyTopCall: false },
+        },
+      ],
+    });
 
-  vm = await client.transport.tevm.getVm();
-  await vm.stateManager.checkpoint();
-});
-
-beforeEach(async () => {
-  await vm.stateManager.revert();
-});
-
-test('state', async () => {
-  await lensClient.contract(callerContract, 'callExternalFunction', []);
-  const txHash = lensClient.callDecodeTracer.tracedTxs.keys().next().value;
-  const callTraceResult = await client.transport.tevm.request({
-    method: 'debug_traceTransaction',
-    params: [
-      {
-        transactionHash: txHash,
-        tracer: 'callTracer',
-        tracerConfig: { onlyTopCall: false },
-      },
-    ],
+    console.log(callTraceResult);
   });
 
-  console.log(callTraceResult);
-});
+  test('deployContract', async () => {
+    await lensClient.contract(callerContract, 'deployContract', []);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('deployContract', async () => {
-  await lensClient.contract(callerContract, 'deployContract', []);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('create2Contract', async () => {
+    const hex32Pattern = ('0x' + '11'.repeat(32)) as Hex;
+    await lensClient.contract(callerContract, 'create2Contract', [hex32Pattern]);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('create2Contract', async () => {
-  const hex32Pattern = ('0x' + '11'.repeat(32)) as Hex;
-  await lensClient.contract(callerContract, 'create2Contract', [hex32Pattern]);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('callExternalFunction', async () => {
+    await lensClient.contract(callerContract, 'callExternalFunction', []);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('callExternalFunction', async () => {
-  await lensClient.contract(callerContract, 'callExternalFunction', []);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('callPublicFunction', async () => {
+    await lensClient.contract(callerContract, 'callPublicFunction', []);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('callPublicFunction', async () => {
-  await lensClient.contract(callerContract, 'callPublicFunction', []);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  // TODO: continue here with decoding result data for fallback & receive
+  test('callWithFallback', async () => {
+    const calldata = '0x20';
+    await lensClient.contract(callerContract, 'callWithFallback', [calldata], ETHER_1);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-// TODO: continue here with decoding result data for fallback & receive
-test('callWithFallback', async () => {
-  const calldata = '0x20';
-  await lensClient.contract(callerContract, 'callWithFallback', [calldata], ETHER_1);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('callReceiveFunction', async () => {
+    await lensClient.contract(callerContract, 'callReceiveFunction', [], ETHER_1);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('callReceiveFunction', async () => {
-  await lensClient.contract(callerContract, 'callReceiveFunction', [], ETHER_1);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('callDelegateCall', async () => {
+    const calldata = '0x'; // Example calldata, adjust as needed
+    await lensClient.contract(callerContract, 'callDelegateCall', [calldata]);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('callDelegateCall', async () => {
-  const calldata = '0x'; // Example calldata, adjust as needed
-  await lensClient.contract(callerContract, 'callDelegateCall', [calldata]);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('callStaticCallViewFunction', async () => {
+    await lensClient.contract(callerContract, 'callStaticCallViewFunction', []);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('callStaticCallViewFunction', async () => {
-  await lensClient.contract(callerContract, 'callStaticCallViewFunction', []);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('testExternalLibCall', async () => {
+    await lensClient.contract(callerContract, 'testExternalLibCall', []);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('testExternalLibCall', async () => {
-  await lensClient.contract(callerContract, 'testExternalLibCall', []);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('testInlineLibCall', async () => {
+    await lensClient.contract(callerContract, 'testInlineLibCall', []);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('testInlineLibCall', async () => {
-  await lensClient.contract(callerContract, 'testInlineLibCall', []);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+  test('callPublicAndExternal', async () => {
+    await lensClient.contract(callerContract, 'callPublicAndExternal', []);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 
-test('callPublicAndExternal', async () => {
-  await lensClient.contract(callerContract, 'callPublicAndExternal', []);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
-
-test('callInternalAndPrivate', async () => {
-  await lensClient.contract(callerContract, 'callInternalAndPrivate', []);
-  inspect(lensClient.callDecodeTracer.tracedTxs);
+  test('callInternalAndPrivate', async () => {
+    await lensClient.contract(callerContract, 'callInternalAndPrivate', []);
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+  });
 });
--- packages/tevm-lens/test/uniswap-v2.test.ts
+++ packages/tevm-lens/test/uniswap-v2.test.ts
@@ -1,7 +1,6 @@
-import { test, beforeEach, beforeAll } from 'vitest';
+import { test, beforeEach, describe } from 'vitest';
 import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
 import { parseEther, tevmSetAccount } from 'tevm';
-import { type Vm } from 'tevm/vm';
 import { deployUniswapV2 } from './setup/uniswap-v2/deploy.ts';
 import { LensClient } from '../src/lens/LensClient.ts';
 import { buildClient } from '../src/lens/client.ts';
@@ -21,148 +20,149 @@ import type { UniswapV2ArtifactsMap } from './setup/uniswap-v2/types.js';
 const ETHER_1 = parseEther('1');
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-let lensClient: LensClient<UniswapV2ArtifactsMap>;
-let factory: Awaited<ReturnType<typeof deployUniswapV2>>['factory'];
-let vm: Vm;
-let client: Awaited<ReturnType<typeof buildClient>>;
-let resourceLoader: IResourceLoader<UniswapV2ArtifactsMap, ProtocolName>;
+describe('uniswap-v2', () => {
+  let lensClient: LensClient<UniswapV2ArtifactsMap>;
+  let factory: Awaited<ReturnType<typeof deployUniswapV2>>['factory'];
+  let client: Awaited<ReturnType<typeof buildClient>>;
+  let resourceLoader: IResourceLoader<UniswapV2ArtifactsMap, ProtocolName>;
 
-beforeAll(async () => {
-  const deployerAccount = privateKeyToAccount(generatePrivateKey());
-  const feeToSetAccount = privateKeyToAccount(generatePrivateKey());
+  beforeEach(async () => {
+    const deployerAccount = privateKeyToAccount(generatePrivateKey());
+    const feeToSetAccount = privateKeyToAccount(generatePrivateKey());
 
-  client = await buildClient(deployerAccount);
+    client = await buildClient(deployerAccount);
 
-  const artifactsPath = path.join(__dirname, '..', '..', 'protocols', 'artifacts');
-  const artifactsContractsPath = path.join(__dirname, '..', '..', 'protocols', 'artifacts', 'contracts');
-  resourceLoader = new TestResourceLoader<UniswapV2ArtifactsMap, ProtocolName>(artifactsPath, artifactsContractsPath);
+    const artifactsPath = path.join(__dirname, '..', '..', 'protocols', 'artifacts');
+    const artifactsContractsPath = path.join(__dirname, '..', '..', 'protocols', 'artifacts', 'contracts');
+    resourceLoader = new TestResourceLoader<UniswapV2ArtifactsMap, ProtocolName>(artifactsPath, artifactsContractsPath);
 
-  const supportedContracts = new SupportedContracts<UniswapV2ArtifactsMap>();
-  const labeledContracts = new DeployedContracts<UniswapV2ArtifactsMap>();
-  const tracer = new LensCallTracer<UniswapV2ArtifactsMap>(supportedContracts, labeledContracts);
-  lensClient = new LensClient<UniswapV2ArtifactsMap>(client, supportedContracts, labeledContracts, tracer);
+    const supportedContracts = new SupportedContracts<UniswapV2ArtifactsMap>();
+    const labeledContracts = new DeployedContracts<UniswapV2ArtifactsMap>();
+    const tracer = new LensCallTracer<UniswapV2ArtifactsMap>(supportedContracts, labeledContracts);
+    lensClient = new LensClient<UniswapV2ArtifactsMap>(client, supportedContracts, labeledContracts, tracer);
 
-  const uniswapV2Artifacts = await resourceLoader.getProtocolArtifacts('uniswap-v2');
-  await supportedContracts.registerArtifacts(uniswapV2Artifacts);
+    const uniswapV2Artifacts = await resourceLoader.getProtocolArtifacts('uniswap-v2');
+    await supportedContracts.registerArtifacts(uniswapV2Artifacts);
 
-  const uniswapFunctionIndexes = await resourceLoader.getFunctionIndexes('uniswap-v2');
-  await supportedContracts.registerFunctionIndexes(uniswapFunctionIndexes);
+    const uniswapFunctionIndexes = await resourceLoader.getFunctionIndexes('uniswap-v2');
+    await supportedContracts.registerFunctionIndexes(uniswapFunctionIndexes);
 
-  await tevmSetAccount(lensClient.client, {
-    address: deployerAccount.address,
-    balance: ETHER_1,
+    await tevmSetAccount(lensClient.client, {
+      address: deployerAccount.address,
+      balance: ETHER_1,
+    });
+
+    const deployment = await deployUniswapV2(lensClient, feeToSetAccount.address, resourceLoader);
+    factory = deployment.factory;
   });
 
-  const deployment = await deployUniswapV2(lensClient, feeToSetAccount.address, resourceLoader);
-  factory = deployment.factory;
+  test('tracer: send success with deployment', async () => {
+    // arrange
+    const token1 = await lensClient.deploy(
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
+      []
+    );
 
-  vm = await client.transport.tevm.getVm();
-  await vm.stateManager.checkpoint();
-});
+    const token2 = await lensClient.deploy(
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
+      []
+    );
 
-beforeEach(async () => {
-  await vm.stateManager.revert();
-});
+    // act
+    await lensClient.contract(factory, 'createPair', [token1.createdAddress!, token2.createdAddress!]);
 
-test('tracer: send success with deployment', async () => {
-  // arrange
-  const token1 = await lensClient.deploy(
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
-    []
-  );
-
-  const token2 = await lensClient.deploy(
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
-    []
-  );
-
-  // act
-  await lensClient.contract(factory, 'createPair', [token1.createdAddress!, token2.createdAddress!]);
-
-  // assert
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
-
-test('tracer: call success', async () => {
-  // arrange
-  const token1 = await lensClient.deploy(
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
-    []
-  );
-  const token2 = await lensClient.deploy(
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
-    []
-  );
-  await lensClient.contract(factory, 'createPair', [token1.createdAddress!, token2.createdAddress!], undefined, false);
-
-  const pairArtifact = await resourceLoader.getArtifact(
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2Pair.sol:UniswapV2Pair'
-  );
-
-  const salt = keccak256(encodePacked(['address', 'address'], [token1.createdAddress!, token2.createdAddress!]));
-  const pairAddress = getContractAddress({
-    bytecode: safeCastToHex(pairArtifact.bytecode),
-    from: factory.address,
-    opcode: 'CREATE2',
-    salt: salt,
+    // assert
+    inspect(lensClient.callDecodeTracer.succeededTxs);
   });
 
-  lensClient.deployedContracts.markContractAddress(
-    pairAddress,
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2Pair.sol:UniswapV2Pair'
-  );
+  test('tracer: call success', async () => {
+    // arrange
+    const token1 = await lensClient.deploy(
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
+      []
+    );
+    const token2 = await lensClient.deploy(
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
+      []
+    );
+    await lensClient.contract(
+      factory,
+      'createPair',
+      [token1.createdAddress!, token2.createdAddress!],
+      undefined,
+      false
+    );
 
-  const pairContract = await lensClient.getContract(
-    pairAddress,
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2Pair.sol:UniswapV2Pair'
-  );
+    const pairArtifact = await resourceLoader.getArtifact(
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2Pair.sol:UniswapV2Pair'
+    );
 
-  // act
-  await lensClient.contract(pairContract, 'getReserves', []);
+    const salt = keccak256(encodePacked(['address', 'address'], [token1.createdAddress!, token2.createdAddress!]));
+    const pairAddress = getContractAddress({
+      bytecode: safeCastToHex(pairArtifact.bytecode),
+      from: factory.address,
+      opcode: 'CREATE2',
+      salt: salt,
+    });
 
-  // assert
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
+    lensClient.deployedContracts.markContractAddress(
+      pairAddress,
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2Pair.sol:UniswapV2Pair'
+    );
 
-test('tracer: call error', async () => {
-  // arrange
-  const token2 = await lensClient.deploy(
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
-    []
-  );
+    const pairContract = await lensClient.getContract(
+      pairAddress,
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2Pair.sol:UniswapV2Pair'
+    );
 
-  // act
-  await lensClient.contract(factory, 'createPair', [ZERO_ADDRESS, token2.createdAddress!]);
+    // act
+    await lensClient.contract(pairContract, 'getReserves', []);
 
-  // assert
-  inspect(lensClient.callDecodeTracer.tracedTxs);
-});
-
-test('debug_traceTransaction', async () => {
-  // arrange
-  const token1 = await lensClient.deploy(
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
-    []
-  );
-
-  const token2 = await lensClient.deploy(
-    'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
-    []
-  );
-
-  // act
-  await lensClient.contract(factory, 'createPair', [token1.createdAddress!, token2.createdAddress!]);
-  const txHash = lensClient.callDecodeTracer.tracedTxs.keys().next().value;
-  const callTraceResult = await client.transport.tevm.request({
-    method: 'debug_traceTransaction',
-    params: [
-      {
-        transactionHash: txHash,
-        tracer: 'callTracer',
-        tracerConfig: { onlyTopCall: false },
-      },
-    ],
+    // assert
+    inspect(lensClient.callDecodeTracer.succeededTxs);
   });
 
-  console.log(callTraceResult);
+  test('tracer: call error', async () => {
+    // arrange
+    const token2 = await lensClient.deploy(
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
+      []
+    );
+
+    // act
+    await lensClient.contract(factory, 'createPair', [ZERO_ADDRESS, token2.createdAddress!]);
+
+    // assert
+    inspect(lensClient.callDecodeTracer.succeededTxs);
+    inspect(lensClient.callDecodeTracer.failedTxs);
+  });
+
+  test('debug_traceTransaction', async () => {
+    // arrange
+    const token1 = await lensClient.deploy(
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
+      []
+    );
+
+    const token2 = await lensClient.deploy(
+      'contracts/uniswap-v2/v2-core/contracts/UniswapV2ERC20.sol:UniswapV2ERC20',
+      []
+    );
+
+    // act
+    await lensClient.contract(factory, 'createPair', [token1.createdAddress!, token2.createdAddress!]);
+    const txHash = lensClient.callDecodeTracer.succeededTxs.keys().next().value;
+    const callTraceResult = await client.transport.tevm.request({
+      method: 'debug_traceTransaction',
+      params: [
+        {
+          transactionHash: txHash,
+          tracer: 'callTracer',
+          tracerConfig: { onlyTopCall: false },
+        },
+      ],
+    });
+
+    console.log(callTraceResult);
+  });
 });
