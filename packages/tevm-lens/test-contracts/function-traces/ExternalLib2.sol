c811166e9dd6c95efe7ec8e9608ba5b0c4ad0391
Nikos Chondromatidis <nchondromatidis@gmail.com>
2026-01-07 12:16:32 +0200

    tevm-lens: removed refactored function-traces test contracts

--- packages/tevm-lens/test-contracts/function-traces/ExternalLib2.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-library ExternalLib2 {
-    function externalOperation(uint value1, uint value2) public pure returns (string memory) {
-        return "Memory function called: externalOperation";
-    }
-}
