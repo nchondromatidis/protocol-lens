5dd0185408a8a34cf2f2e717b206636696330683
Nikos Chondromatidis <nchondromatidis@gmail.com>
2026-03-24 19:56:45 +0200

    lens: allowed evm to accept contracts above size limit

--- packages/evm-lens/src/adapters/client.ts
+++ packages/evm-lens/src/adapters/client.ts
@@ -10,6 +10,7 @@ import {
   type Chain,
 } from 'viem';
 import { localhost } from 'viem/chains';
+import { createEvm } from 'tevm/evm';
 
 export type PublicTestClient<
   TTransport extends Transport = Transport,
@@ -28,7 +29,21 @@ export async function buildClient(defaultAccount: Account): Promise<PublicTestCl
   })
     .extend(publicActions) // adds getBalance, getBlock, etc.
     .extend(testActions({ mode: 'anvil' })); // adds mine, setBalance, etc.;
+
   await tevmReady(client);
 
+  await allowContractsAboveSizeLimit(client);
+
   return client;
+}
+
+async function allowContractsAboveSizeLimit(client: Client<TevmTransport>) {
+  const node = client.transport.tevm;
+  const vm = await node.getVm();
+  vm.evm = await createEvm({
+    common: vm.common,
+    stateManager: vm.stateManager,
+    blockchain: vm.blockchain,
+    allowUnlimitedContractSize: true,
+  });
 }
